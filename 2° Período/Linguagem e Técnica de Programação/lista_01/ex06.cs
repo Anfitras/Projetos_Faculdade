f53a3510c015a160f5b6269c23dff58f83c78b61
Arthur Beraldo <arthur060307@gmail.com>
2025-09-09 22:57:10 -0300

    Commit Lista 01

---
+++ 2° Período/Linguagem e Técnica de Programação/lista_01/ex06.cs
@@ -0,0 +1,56 @@
+using System;
+
+class ex06
+{
+    static void Main()
+    {
+        int tamanhoVetor, numUsuario;
+
+        Console.Write("Tamanho do vetor: ");
+        tamanhoVetor = int.Parse(Console.ReadLine());
+
+        int[] vetor = new int[tamanhoVetor];
+
+        Random rand = new Random();
+        for (int i = 0; i < tamanhoVetor; i++)
+        {
+            vetor[i] = rand.Next(0, 100);
+        }
+
+        Console.WriteLine("--- Vetor ---");
+
+        for (int i = 0; i < tamanhoVetor; i++) {
+            Console.Write(vetor[i] + "|");
+        }
+
+        Console.WriteLine();
+
+        while (true)
+        {
+            int confirm = 0;
+            int pos = 0;
+
+            Console.WriteLine("Digite um valor: ");
+            numUsuario = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < tamanhoVetor; i++)
+            {
+                if (numUsuario == vetor[i])
+                {
+                    confirm = 1;
+                    pos = i;
+                }
+            }
+
+            if (confirm == 1)
+            {
+                Console.WriteLine($"Este valor está no vetor! Na posição {pos}°.");
+            }
+            else
+            {
+                Console.WriteLine("Este valor não está no vetor!");
+            }
+
+        }
+    }
+}
---
+++ 2° Período/Linguagem e Técnica de Programação/semana_06/Matrizes/Matriz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MinhaBiblioteca;
+
+class Matriz
+{
+    static void Main()
+    {
+        int linhas, colunas;
+
+        Console.Write("Quantidade de linhas: ");
+        linhas = int.Parse(Console.ReadLine());
+        Console.Write("Quantidade de colunas: ");
+        colunas = int.Parse(Console.ReadLine());
+
+        int[,] matriz = new int[linhas, colunas];
+
+        Biblioteca.gerarMatriz(matriz);
+
+        Biblioteca.escreverMatriz(matriz);
+
+
+    }
+}
