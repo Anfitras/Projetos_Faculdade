73fc46e8d255c22ebaf10fac312c06da25ade14e
Arthur Beraldo <arthur060307@gmail.com>
2025-09-16 00:52:11 -0300

    2° Lista

---
+++ 2° Período/Linguagem e Técnica de Programação/lista_02/ex02.cs
@@ -0,0 +1,39 @@
+using System;
+using MinhaBiblioteca;
+class ex02
+{
+    static int getMax(int[,] matriz)
+    {
+        int maior = matriz[0, 0];
+        int linhas = matriz.GetLength(0);
+        int colunas = matriz.GetLength(1);
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                if (maior < matriz[i, j])
+                    maior = matriz[i, j];
+            }
+        }
+
+            return maior;
+    }
+    static void Main()
+    {
+        int linhas, colunas;
+
+        Console.WriteLine("Entre com a quantidade de linhas e colunas: ");
+        linhas = int.Parse(Console.ReadLine());
+        colunas = int.Parse(Console.ReadLine());
+
+        int[,] matriz = new int[linhas, colunas];
+
+        Biblioteca.gerarMatriz(matriz);
+        Biblioteca.escreverMatriz(matriz);
+
+        Console.WriteLine("----- Maior -----");
+
+        Console.WriteLine(getMax(matriz));
+    }
+}
---
+++ 2° Período/Linguagem e Técnica de Programação/lista_02/ex03.cs
@@ -0,0 +1,41 @@
+using System;
+using MinhaBiblioteca;
+class ex03
+{
+    static int getMax(int[,] matriz, int x)
+    {
+        int contador = 0;
+        int linhas = matriz.GetLength(0);
+        int colunas = matriz.GetLength(1);
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                if (x == matriz[i, j])
+                    contador++;
+            }
+        }
+
+        return contador;
+    }
+    static void Main()
+    {
+        int linhas, colunas, x;
+
+        Console.WriteLine("Entre com a quantidade de linhas e colunas: ");
+        linhas = int.Parse(Console.ReadLine());
+        colunas = int.Parse(Console.ReadLine());
+        Console.Write("Valor x: ");
+        x = int.Parse(Console.ReadLine());
+
+        int[,] matriz = new int[linhas, colunas];
+
+        Biblioteca.gerarMatriz(matriz);
+        Biblioteca.escreverMatriz(matriz);
+
+        Console.WriteLine("----- Aparições de X -----");
+
+        Console.WriteLine(getMax(matriz, x));
+    }
+}
