9b5e2da4d0dd31a62b7effcafbd5a6c6c71f0d23
ARTHUR SERAPIAO BERALDO <13789459640@CAMPUS.MACHADO.MCH>
2025-10-07 22:01:56 -0300

    Prova

---
+++ 2° Período/Linguagem e Técnica de Programação/Prova/exercicio1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+// Arthur Beraldo (T1)
+class exercicio1
+{
+	static void carregarArquivo(int[,] matriz, string nomeArquivo = "mapa.txt")
+    {
+        string[] dados;
+        try
+        {
+            dados = System.IO.File.ReadAllLines(nomeArquivo);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao ler o arquivo: " + ex.Message);
+            return;
+        }
+        int numLinhas = matriz.GetLength(0);
+        int numCols = matriz.GetLength(1);
+        for (int i = 0; i < numLinhas && i < dados.Length; i++)
+        {
+            string[] linha = dados[i].Split(',');
+            for (int j = 0; j < numCols && j < linha.Length; j++)
+            {
+                matriz[i, j] = int.Parse(linha[j]);
+            }
+        }
+    }
+	
+	static void mostrarMatriz(int[,] matriz)
+    {
+        int linhas = matriz.GetLength(0);
+        int cols = matriz.GetLength(1);
+        Console.WriteLine("*** Matriz  de Marte ***");
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"{matriz[i, j],2} | ");
+            }
+            Console.WriteLine();
+        }
+    }
+	
+	static int passosAoMineral(int[,] matriz)
+	{	
+		int passos = 0;
+		int totalPassos = 0;
+		int linhas = matriz.GetLength(0);
+		int cols = matriz.GetLength(1);
+		int linhaParaPular = 0;
+		bool achou = false;
+		
+		for (int i = 0; i < linhas; i++)
+		{
+			for (int j = linhaParaPular; j < cols; j++)
+			{
+				Console.WriteLine($"Analisando posição [{i},{j}]");
+				passos++;
+				
+				if(matriz[i,j] == 2)
+				{
+					Console.WriteLine($"Indício encontrado na posição [{i},{j}], descendo para o sul");
+					linhaParaPular = j + 1;
+					break;
+				}
+				else if(matriz[i,j] == 5)
+				{
+					Console.WriteLine($"Amostra rara encontrado na posição [{i},{j}]");
+					totalPassos = passos;
+					achou = true;
+					break;
+				}
+				
+				linhaParaPular = 0;
+			}
+			
+			if (achou == true)
+			{
+				break;
+			}
+		}
+		
+		if (achou == false)
+		{
+			totalPassos = -1;
+		}
+		
+		return totalPassos;
+	}
+	
+	static void Main()
+	{
+		{
+			int[,] matriz = new int [5,5];
+			carregarArquivo(matriz);
+			mostrarMatriz(matriz);
+			
+			int totalPassos = passosAoMineral(matriz);
+			
+			if (totalPassos == -1)
+			{
+				Console.WriteLine("Mineral raro não encontrado");
+			}
+			else
+			{
+				Console.WriteLine($"Total de passsos: {totalPassos}");
+			}
+		}
+	}
+}
---
+++ 2° Período/Linguagem e Técnica de Programação/Prova/exercicio2.cs
@@ -0,0 +1,89 @@
+﻿using System;
+//Arthur Beraldo (T1)
+class exercicio2
+{
+	static void carregarArquivo(double[] vetor, string nomeArquivo = "temp.txt")
+    {
+        string dados;
+        try
+        {
+            dados = System.IO.File.ReadAllText(nomeArquivo);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro ao ler o arquivo: " + ex.Message);
+            return;
+        }
+       
+	   string[] elementos = dados.Split(';');
+        int tamanho = vetor.Length;
+
+        for (int i = 0; i < tamanho && i < elementos.Length; i++)
+        {
+            vetor[i] = double.Parse(elementos[i].Trim());
+        }
+    }
+	
+	static void mostrarVetor(double[] vetor)
+    {
+        int tamanho = vetor.Length;
+        Console.WriteLine("*** Vetor de Temperaturas ***");
+        for (int i = 0; i < tamanho; i++)
+        {
+            Console.Write($"{vetor[i]:F2} ");
+        }
+        Console.WriteLine();
+    }
+	
+	static double mediaTemp(double[] vetor)
+	{
+		int tamanho = vetor.Length;
+		double soma = 0.0;
+		
+		for (int i = 0; i < tamanho; i++)
+		{
+			soma += vetor[i];
+		}
+		
+		double media = (soma / vetor.Length);
+		
+		return media;
+	}
+	
+	static int diasEntreTemp(double[] vetor)
+	{
+		int tamanho = vetor.Length;
+		int totalDias = 0;
+		
+		for (int i = 0; i < tamanho; i++)
+		{
+			if (vetor[i] >= 22.0 && vetor[i] <= 25.0)
+			{
+				totalDias++;
+			}
+		}
+		
+		return totalDias;
+	}
+		
+	static void Main()
+	{
+		double[] clima = new double[30];
+		
+		carregarArquivo(clima);
+		
+		mostrarVetor(clima);
+		
+		double mediaTemperatura = mediaTemp(clima);
+		
+		Console.WriteLine($"Média mensal das temperaturas: {mediaTemperatura:F2}");
+		
+		if (mediaTemperatura > 22.0)
+		{
+			Console.WriteLine("Alerta de temperatura média mensal - maior que 22 graus");
+		}
+		
+		Console.WriteLine($"Total de dias com temperatura entre 22 e 25 graus: {diasEntreTemp(clima)}");
+		
+	}
+}
