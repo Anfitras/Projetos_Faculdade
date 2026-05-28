e334064a3030d74305fb3b0ecf2078f3c1801a92
Anfitras <arthur060307@gmail.com>
2026-05-28 02:54:27 -0300

    Produtos

--- 3° Período/POO/produto/src/main/java/com/ifsuldeminas/produto/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ifsuldeminas.produto.repository;
-
-import com.ifsuldeminas.produto.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
