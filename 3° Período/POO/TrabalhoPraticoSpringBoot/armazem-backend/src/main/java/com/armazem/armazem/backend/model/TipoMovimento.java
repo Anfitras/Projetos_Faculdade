80b4d4b7f708f03dfe616543493f3e9f62490ab3
Anfitras <arthur060307@gmail.com>
2026-06-21 14:30:59 -0300

    Ajuste

--- 3° Período/POO/TrabalhoPraticoSpringBoot/armazem-backend/src/main/java/com/armazem/armazem/backend/model/TipoMovimento.java
+++
@@ -1,6 +0,0 @@
-package com.armazem.armazem.backend.model;
-
-public enum TipoMovimento {
-    ENTRADA,
-    SAIDA
-}
--- 3° Período/POO/TrabalhoPraticoSpringBoot/armazem-backend/src/main/java/com/armazem/armazem/backend/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.armazem.armazem.backend.repository;
-
-import com.armazem.armazem.backend.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
